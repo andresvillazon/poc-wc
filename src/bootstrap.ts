d24c984b40785dd4394642f90a464728e340b291
Andrés Villazón <andres.villazon@keifitech.com>
2025-11-03 16:56:45 +0100

    v3.5

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -4,11 +4,9 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 import { enableProdMode } from '@angular/core';
-console.log('09')
 if (environment.NODE_ENV !== 'production') {
     bootstrapApplication(AppComponent, appConfig)
     .catch((err) => console.error(err));
 }else{
-    console.log('10')
     enableProdMode();
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,3 @@
-// import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
-// import { ApplicationRef } from '@angular/core';
-// import { createCustomElement } from '@angular/elements';
-// import { createApplication } from '@angular/platform-browser';
-// import { provideRouter } from '@angular/router';
-
-// import { bootstrapApplication } from "@angular/platform-browser";
-// import { AppComponent } from "./app/app.component";
-// import { appConfig } from "./app/app.config";
-
 import { environment } from './environments/environment';
 import { WebComponent } from './app/components/web-component/web-component';
 import { provideHttpClient } from '@angular/common/http';
@@ -18,9 +8,7 @@ import { provideRouter } from '@angular/router';
 
 import('./bootstrap')
   .then(async () => {
-    console.log('hola ')
     if (environment.NODE_ENV === 'production') {
-      console.log('h2222la ')
       const appRef: ApplicationRef = await createApplication({
         providers: [
           provideHttpClient(),
